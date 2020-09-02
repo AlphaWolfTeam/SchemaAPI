2ba77ab64a59559fb344729e5a30be4b9e8d008d
vitaly-furman <furman4488@gmail.com>
2020-09-02 11:11:45 +0300

    try

--- server/server.ts
+++ server/server.ts
@@ -5,5 +5,5 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 app.listen(3000, function () {
-    console.log('App is listening on port 3000!');
+    console.log('Carmel');
 });
