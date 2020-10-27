79cd427061fc4184130344eb54c5516c79306dff
CarmelTsadok <carmelcarmel88888@gmail.com>
2020-10-27 20:35:22 +0200

    fix(router.ts): Fix routes

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import schemaRouter from "./schema/schema.router";
 
 const appRouter: Router = Router();
-appRouter.use(schemaRouter);
+appRouter.use("/api/schema", schemaRouter);
+// TODO: Add client native
+appRouter.use("/", schemaRouter);
 
 export default appRouter;
--- src/schema/schema.router.ts
+++ src/schema/schema.router.ts
@@ -7,32 +7,32 @@ import { config } from "../utils/permissions/permissions.config";
 const schemaRouter: Router = Router();
 
 schemaRouter.post(
-  "/api/schema",
+  "/",
   wrapAsyncPermissions(checkPermission, config.create),
   wrapAsync(Controller.create)
 );
 schemaRouter.put(
-  "/api/schema/:id",
+  "/:id",
   wrapAsyncPermissions(checkPermission, config.update),
   wrapAsync(Controller.update)
 );
 schemaRouter.delete(
-  "/api/schema/:id",
+  "/:id",
   wrapAsyncPermissions(checkPermission, config.deleteSchema),
   wrapAsync(Controller.deleteSchema)
 );
 schemaRouter.delete(
-  "/api/schema/:id/:propertyId",
+  "/:id/:propertyId",
   wrapAsyncPermissions(checkPermission, config.deleteProperty),
   wrapAsync(Controller.deleteProperty)
 );
 schemaRouter.get(
-  "/api/schema/:id",
+  "/:id",
   wrapAsyncPermissions(checkPermission, config.getById),
   wrapAsync(Controller.getById)
 );
 schemaRouter.get(
-  "/api/schema",
+  "/",
   wrapAsyncPermissions(checkPermission, config.getAll),
   wrapAsync(Controller.getAll)
 );
