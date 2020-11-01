d1a03eaff28b7e16bd72bbdf22bd6f95883993bd
may kremer <maykremer@gmail.com>
2020-11-01 15:02:34 +0200

    fix(node_modules): Fix ts-node

--- dist/router.js
+++ dist/router.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const schema_router_1 = __importDefault(require("./schema/schema.router"));
 const appRouter = express_1.Router();
-appRouter.use(schema_router_1.default);
+appRouter.use("/api/schema", schema_router_1.default);
+appRouter.use("/", schema_router_1.default);
 exports.default = appRouter;
 //# sourceMappingURL=router.js.map
